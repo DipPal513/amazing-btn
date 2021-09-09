361b8bbb1813244a93cddbf5155fda8ca4ca3f61
DipPal513 <65772938+DipPal513@users.noreply.github.com>
2021-09-09 17:49:29 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('button');
+
+btn.onmousemove = function(e) {
+    let x = e.pageX - btn.offsetLeft;
+    let y = e.pageY - btn.offsetTop;
+    btn.style.setProperty('--x',x + 'px');
+    btn.style.setProperty('--y',y + 'px');
+}
